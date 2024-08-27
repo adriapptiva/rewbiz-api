2d78b74483c170d9bec46dda30d520e216d2be6b
adriapptiva <adrian.lopez@apptivatech.com>
2024-08-27 12:24:34 +0200

    new routes

--- controllers/collectionController.js
+++ controllers/collectionController.js
@@ -1,10 +1,159 @@
-// controllers/collectionController.js
 const { MongoClient, ObjectId } = require('mongodb');
+const axios = require('axios'); // Asegúrate de añadir esta línea
 const url = process.env.MONGODB_URI; // URL de conexión a MongoDB desde tu archivo .env
+
+
+// Guardar el usuario
+const saveUser = async (req, res) => {
+  const userData = req.body;
+  let client;
+
+  try {
+    client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    await client.connect();
+    const db = client.db();
+    const usersCollection = db.collection('users');
+
+    // Verificar si el usuario ya existe
+    const existingUser = await usersCollection.findOne({ uid: userData.uid });
+
+    if (existingUser) {
+      // Actualizar el usuario existente
+      await usersCollection.updateOne(
+        { uid: userData.uid },
+        { $set: userData }
+      );
+      res.status(200).json({ message: 'Usuario actualizado con éxito' });
+    } else {
+      // Insertar nuevo usuario
+      await usersCollection.insertOne(userData);
+      res.status(201).json({ message: 'Usuario guardado con éxito' });
+    }
+  } catch (error) {
+    console.error('Error al guardar el usuario:', error.message);
+    res.status(500).json({ error: 'Internal Server Error', message: 'Error al guardar el usuario' });
+  } finally {
+    if (client) {
+      await client.close();
+    }
+  }
+};
+
+// Get user by Email
+const getUserByEmail = async (req, res) => {
+  const { email } = req.params;
+  let client;
+
+  try {
+    client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    await client.connect();
+    const db = client.db();
+    const usersCollection = db.collection('users');
+
+    const user = await usersCollection.findOne({ email: email });
+
+    if (!user) {
+      return res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+
+    // Buscar el documento correspondiente en todo_restaurantes_españa
+    const restaurantesCollection = db.collection('todo_restaurantes_españa');
+    const business = await restaurantesCollection.findOne({ id_negocio: user.id_negocio });
+
+    res.status(200).json({
+      ...user,
+      businessId: business ? business._id.toString() : null
+    });
+  } catch (error) {
+    console.error('Error al obtener el usuario:', error.message);
+    res.status(500).json({ error: 'Internal Server Error', message: 'Error al obtener el usuario' });
+  } finally {
+    if (client) {
+      await client.close();
+    }
+  }
+};
+
+
+// Update user
+const updateUser = async (req, res) => {
+  const { email, id_negocio, nombre_usuario, telefono, mail_contacto, direccion } = req.body;
+  let client;
+
+  try {
+    client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
+    const db = client.db();
+    const usersCollection = db.collection('users');
+
+    // Construir el objeto $set dinámicamente
+    const updateFields = { id_negocio: id_negocio };
+
+    if (nombre_usuario) {
+      updateFields.nombre_usuario = nombre_usuario;
+    }
+    if (telefono) {
+      updateFields.telefono = telefono;
+    }
+    if (mail_contacto) {
+      updateFields.mail_contacto = mail_contacto;
+    }
+    if (direccion) {
+      updateFields.direccion = direccion;
+    }
+
+    const result = await usersCollection.updateOne(
+      { email: email },
+      { $set: updateFields }
+    );
+
+    if (result.modifiedCount === 0) {
+      return res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+
+    res.status(200).json({ message: 'Usuario actualizado con éxito' });
+  } catch (error) {
+    console.error('Error al actualizar el usuario:', error.message);
+    res.status(500).json({ error: 'Internal Server Error', message: 'Error al actualizar el usuario' });
+  } finally {
+    if (client) {
+      await client.close();
+    }
+  }
+};
+
+
+// Función para generar un informe con OpenAI
+const openaiApiKey = process.env.OPENAI_API_KEY;
+
+const generateReportWithOpenAI = async (req, res) => {
+  try {
+    const data = req.body; // Obtén los datos del cuerpo de la solicitud
+
+    // Llamada a la API de OpenAI
+    const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+      model: 'text-davinci-003', // Asegúrate de que el modelo sea válido
+      prompt: `Genera un informe detallado sobre el siguiente negocio:\n${JSON.stringify(data)}`,
+      max_tokens: 150
+    }, {
+      headers: {
+        'Authorization': `Bearer ${openaiApiKey}`,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    // Devuelve el informe generado como respuesta
+    res.status(200).json({ report: response.data.choices[0].text });
+  } catch (error) {
+    console.error('Error al generar el informe:', error.message); // Imprime el mensaje de error
+    res.status(500).json({ message: 'Error al generar el informe', error: error.message });
+  }
+};
 
 // Función para obtener un documento por ID en una colección específica
 const getItemById = async (req, res) => {
-  console.log("GET ITEM BY ID");
   const { collection, id } = req.params;
   let client;
 
@@ -15,7 +164,6 @@ const getItemById = async (req, res) => {
     const db = client.db();
 
     const collectionNames = await db.listCollections().toArray();
-    console.log("collectionNames: ", collectionNames);
     const collectionExists = collectionNames.some(col => col.name === collection);
 
     if (!collectionExists) {
@@ -31,7 +179,7 @@ const getItemById = async (req, res) => {
 
     res.json(item);
   } catch (error) {
-    console.error("Error al obtener el documento:", error);
+    console.error("Error al obtener el documento:", error.message); // Imprime el mensaje de error
     res.status(500).json({ error: "Internal Server Error", message: "Error interno del servidor" });
   } finally {
     if (client) {
@@ -53,7 +201,6 @@ const getNearbyBusinesses = async (req, res) => {
     const db = client.db();
 
     const collectionRef = db.collection(collection); // Usar el nombre de la colección del parámetro
-    console.log(`Consulta con parámetros: lat=${lat}, lng=${lng}, radius=${radiusInMeters}, collection=${collection}`);
 
     const nearbyBusinesses = await collectionRef.find({
       coordinatesGeoJSON: {
@@ -63,10 +210,9 @@ const getNearbyBusinesses = async (req, res) => {
       },
     }).toArray();
 
-    console.log(`Negocios encontrados: ${nearbyBusinesses.length}`);
     res.json(nearbyBusinesses);
   } catch (error) {
-    console.error("Error al obtener los comercios cercanos:", error);
+    console.error("Error al obtener los comercios cercanos:", error.message); // Imprime el mensaje de error
     res.status(500).json({ error: "Internal Server Error", message: "Error interno del servidor" });
   } finally {
     if (client) {
@@ -75,7 +221,83 @@ const getNearbyBusinesses = async (req, res) => {
   }
 };
 
+// Función para validar el ID de negocio
+const validateIdNegocio = async (req, res) => {
+  let { id } = req.params;
+  id = decodeURIComponent(id);
+  let client;
+
+  try {
+    client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    await client.connect();
+    const db = client.db();
+    
+    // Asumiendo que 'todo_restaurantes_españa' es el nombre correcto de tu colección
+    const collection = db.collection('todo_restaurantes_españa');
+
+    const business = await collection.findOne({ id_negocio: id });
+
+    if (business) {
+      res.status(200).json({ isValid: true, message: 'ID de negocio válido' });
+    } else {
+      res.status(200).json({ isValid: false, message: 'ID de negocio no encontrado' });
+    }
+  } catch (error) {
+    console.error('Error al validar el ID de negocio:', error.message);
+    res.status(500).json({ error: 'Internal Server Error', message: 'Error al validar el ID de negocio' });
+  } finally {
+    if (client) {
+      await client.close();
+    }
+  }
+};
+
+// Guardar un informe en mongo
+// controllers/collectionController.js
+
+const saveReport = async (req, res) => {
+  const { id_negocio, id_informe, fecha_informe, texto } = req.body;
+  let client;
+
+  try {
+    client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    await client.connect();
+    const db = client.db();
+    const informesCollection = db.collection('informes');
+
+    // Crea el objeto del informe con el estado inicial en "processing"
+    const newReport = {
+      id_negocio: id_negocio,
+      id_informe: id_informe,
+      fecha_informe: fecha_informe,
+      texto: texto,
+      status: 'processing'
+    };
+
+    // Inserta el nuevo informe en la colección
+    const result = await informesCollection.insertOne(newReport);
+
+    // Devuelve el ID del documento insertado
+    res.status(201).json({ message: 'Informe guardado con éxito', _id: result.insertedId });
+  } catch (error) {
+    console.error('Error al guardar el informe:', error.message);
+    res.status(500).json({ error: 'Internal Server Error', message: 'Error al guardar el informe' });
+  } finally {
+    if (client) {
+      await client.close();
+    }
+  }
+};
+
 module.exports = {
+  saveUser,
+  getUserByEmail,
+  updateUser,
+  generateReportWithOpenAI,
   getItemById,
   getNearbyBusinesses,
+  validateIdNegocio,
+  saveReport
 };
--- routes/collectionRoutes.js
+++ routes/collectionRoutes.js
@@ -3,10 +3,29 @@ const express = require("express");
 const router = express.Router();
 const collectionController = require("../controllers/collectionController");
 
+// Ruta para guardar usuarios
+router.post('/save-user', collectionController.saveUser);
+
+// Ruta para update del usuario
+router.post('/update-user', collectionController.updateUser);
+
+// Ruta para get user
+router.get('/user/:email', collectionController.getUserByEmail);
+
+// Ruta para generar informe básico
+router.post('/generate-report', collectionController.generateReportWithOpenAI);
+
+// Ruta para validar el ID de negocio
+router.get('/validate-id-negocio/:id', collectionController.validateIdNegocio);
+
 // Ruta para obtener un documento por ID en una colección específica
 router.get("/:collection/:id", collectionController.getItemById);
 
 // Ruta para obtener negocios cercanos
 router.get("/nearby-businesses", collectionController.getNearbyBusinesses);
+
+// Ruta para guardar informes
+router.post('/save-report', collectionController.saveReport);
+
 
 module.exports = router;
